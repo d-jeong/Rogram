b48fd2f3fe817e7b16567972145be74b2a0314cb
David Jeong <djeong@lyft.com>
2021-06-13 02:34:36 -0400

    Initial Commit

---
+++ Rogram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rogram
+//
+//  Created by David Jeong on 6/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rogram/RogramApp.swift
@@ -0,0 +1,17 @@
+//
+//  RogramApp.swift
+//  Rogram
+//
+//  Created by David Jeong on 6/13/21.
+//
+
+import SwiftUI
+
+@main
+struct RogramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
